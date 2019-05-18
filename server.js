05a662a285287b7af9ef78dadee72856c808e361
Otmane BENAZZOU <obenazzo@e1r1p14.42.fr>
2019-05-18 23:09:55 +0200

    replacing websockets with rest api

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var bodyParser = require('body-parser')
 var http = require('http');
 var fs = require("fs");
-var db = fs.existsSync(__dirname + "/data.json") ? JSON.parse(fs.readFileSync(__dirname + "/data.json").toString()) : {};
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-
+var Datastore = require('nedb');
+// var io = require('socket.io')(server);
+function tmp_err(res, err)
+{
+	console.error("ERROR", err)
+	res.redirect("/");
+}
+var db = {
+	actions: new Datastore({ filename: __dirname + '/actions_db' })
+}
+db.actions.loadDatabase();
+var API_PATH = '/API'
 app
 .get('/', function (req, res) {
 	res.send('Hello World')
@@ -20,79 +29,99 @@ app
 .use(bodyParser.urlencoded({
 	extended: true
 }))
-.post('/habit/add', function (req, res) {
-	console.log(req.body);
- 	var tmp = {name: req.body.name,
-		duration: parseFloat(req.body.duration),
-		priority: parseFloat(req.body.priority),
-		split: parseFloat(req.body.split),
-		min: parseInt(req.body.min.split(":")[0]) + req.body.min.split(":")[1] / 60,
-		max: parseInt(req.body.max.split(":")[0]) + req.body.max.split(":")[1] / 60,
-		week: [!!req.body['Monday'], !!req.body['Tuesday'], !!req.body['Wednesday'], !!req.body['Thursday'], !!req.body['Friday'], !!req.body['Saturday'], !!req.body['Sunday']]
+.post(API_PATH + '/actions', function (req, res) {
+	try {
+		var tmp = {
+			name: req.body.name,
+			time_goal: parseFloat(req.body.duration),
+			priority: parseFloat(req.body.priority),
+			minimum_duration: parseFloat(req.body.split),
+			availability: {
+				//this is garbage
+				min: parseInt(req.body.min.split(":")[0]) + req.body.min.split(":")[1] / 60,
+				max: parseInt(req.body.max.split(":")[0]) + req.body.max.split(":")[1] / 60,
+				week: [!!req.body['Monday'], !!req.body['Tuesday'], !!req.body['Wednesday'], !!req.body['Thursday'], !!req.body['Friday'], !!req.body['Saturday'], !!req.body['Sunday']]
+			}
+		}
+		db.actions.insert(tmp, function (err, newDoc) {
+			if (err) return tmp_err(res, err);
+			console.log('Added new habit.', newDoc);
+			res.status(201);
+			res.end();
+		});
 	}
-	console.log(tmp)
-	res.send('Hello World')
+	catch (err) { return tmp_err(res, err); }
+})
+.get(API_PATH + '/actions', function (req, res) {
+	try {
+		var tmp_id = "LPa8n7FtrVvXZMTU";//TODO:replace with auth' user id
+		db.actions.find({ user_id: tmp_id }, function (err, docs) {
+			if (err) return tmp_err(res, err);
+			res.send(JSON.stringify(docs));
+		});
+	}
+	catch (err) { return tmp_err(res, err); }
 });
-function habitReminder(habits, hours)
+function habitReminder(actions, hours)
 {
-	habits = JSON.parse(JSON.stringify(habits))
-	var tmp = habits.filter(h => h.min <= hours && h.max >= hours && h.progress < h.duration);
+	actions = JSON.parse(JSON.stringify(actions))
+	var tmp = actions.filter(h => h.min <= hours && h.max >= hours && h.progress < h.duration);
 	tmp.sort((a, b) => a.priority > b.priority);
 	tmp = tmp.filter(x => x.priority == tmp[0].priority);
 	tmp.sort((a, b) => ((a.split < (a.duration - a.progress)) ? a.split : (a.duration - a.progress)) - ((b.split < (b.duration - b.progress)) ? b.split : (b.duration - b.progress)));
 	return (tmp);
 }
-var io = require('socket.io').listen(server);
-
-io.sockets.on('connection', function (socket) {
-	socket.on('ask_recommendations', function (data) {
-		socket.emit('new_recommendations', habitReminder(db[data.login].habits, data.hours))
-	});
-	socket.on('add_habit', function (data) {
-		try {
-			if (!db[data.pseudo])
-				db[data.pseudo] = {habits: [], locked: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
-		//TODO: IMPORTANT, SANITIZE INPUT U LAZY MORON
-			db[data.pseudo].habits[data.habit.name] = data.habit;
-		}
-		catch(err)
-		{
-			console.error(err)
-			socket.emit('error', 'add_habit_error');
-		}
-	});
-	socket.on('remove_habit', function (data) {
-		try {
-			delete db[data.pseudo].habits[data.habit];
-		}
-		catch(err)
-		{
-			console.error(err);
-			socket.emit('error', 'remove_habit_error');
-		}
-	});
-	socket.on('choose_habit', function (data) {
-		db[data.pseudo]
-		try {
-			db[data.pseudo].habits[data.habit].progress -= data.duration;
-		}
-		catch(err)
-		{
-			console.error(err);
-			socket.emit('error', 'choose_habit_error');
-		}
-	});
-	socket.on('lock_time', function (data) {
-		try {
-			db[data.pseudo].locked = data.locked
-		}
-		catch(err)
-		{
-			console.error(err);
-			socket.emit('error', 'lock_time_error');
-		}
-	});
-});
+// var io = require('socket.io').listen(server);
+//
+// io.sockets.on('connection', function (socket) {
+// 	socket.on('ask_recommendations', function (data) {
+// 		socket.emit('new_recommendations', habitReminder(db[data.login].actions, data.hours))
+// 	});
+// 	socket.on('add_habit', function (data) {
+// 		try {
+// 			if (!db[data.pseudo])
+// 				db[data.pseudo] = {actions: [], locked: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
+// 		//TODO: IMPORTANT, SANITIZE INPUT U LAZY MORON
+// 			db[data.pseudo].actions[data.habit.name] = data.habit;
+// 		}
+// 		catch(err)
+// 		{
+// 			console.error(err)
+// 			socket.emit('error', 'add_habit_error');
+// 		}
+// 	});
+// 	socket.on('remove_habit', function (data) {
+// 		try {
+// 			delete db[data.pseudo].actions[data.habit];
+// 		}
+// 		catch(err)
+// 		{
+// 			console.error(err);
+// 			socket.emit('error', 'remove_habit_error');
+// 		}
+// 	});
+// 	socket.on('choose_habit', function (data) {
+// 		db[data.pseudo]
+// 		try {
+// 			db[data.pseudo].actions[data.habit].progress -= data.duration;
+// 		}
+// 		catch(err)
+// 		{
+// 			console.error(err);
+// 			socket.emit('error', 'choose_habit_error');
+// 		}
+// 	});
+// 	socket.on('lock_time', function (data) {
+// 		try {
+// 			db[data.pseudo].locked = data.locked
+// 		}
+// 		catch(err)
+// 		{
+// 			console.error(err);
+// 			socket.emit('error', 'lock_time_error');
+// 		}
+// 	});
+// });
 
 server.listen(8080);
 
